654a94a43c982f958be222c42e5cdf1526dff4c8
agasconn <alex.gascon1999@gmail.com>
2025-10-08 11:11:32 +0200

    Podcasts

--- js/podcasts.js
+++ js/podcasts.js
@@ -1,5 +1,8 @@
 // js/podcasts.js
-import { createOrUpdateChart, chartColors } from './charts.js';
+import { createOrUpdateChart } from './charts.js'; // quitamos chartColors
+
+// Definimos los colores localmente
+const chartColors = ['#4e79a7', '#f28e2b', '#e15759', '#76b7b2', '#59a14f', '#edc948', '#b07aa1', '#ff9da7', '#9c755f', '#bab0ab'];
 
 export function analyzePodcasts(fullData) {
     // Filtrar solo podcasts
